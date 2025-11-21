b63cc793006338da2af998ac1c6231aaaff31d85
Abhi-Kurne <abhishek.kurne@pacewisdom.com>
2025-11-21 18:06:49 +0530

    added invitation status col

--- src/types/super-admin/user-management/form-type.ts
+++ src/types/super-admin/user-management/form-type.ts
@@ -6,5 +6,6 @@ export type UserRow = {
   first_name?: string;
   last_name?: string;
   status: "Active" | "Awaiting" | "Deactivated";
+  invitation_status: string;
   invitation_id?: string | null;
 };
